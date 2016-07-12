61551f8e0c711098fac9f51de18753be950a4566
Nicole Tan <nat34@cornell.edu>
2016-07-12 12:19:54 -0400

    Mimicking Roll A Ball tutorial to incorporate CSV tracking

---
+++ Virtual Reality Test/Assets/_Scripts/TestScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR;
+using System; 
+using System.Runtime.InteropServices;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
