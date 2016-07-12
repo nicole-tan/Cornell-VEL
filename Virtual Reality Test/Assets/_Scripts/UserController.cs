3d74ac246fbfc4b6a69ba6ffaf8e3309ac9cb443
Nicole Tan <nat34@cornell.edu>
2016-07-12 14:10:30 -0400

    Keeping track of amount of time passing in csv file

--- Virtual Reality Test/Assets/_Scripts/UserController.cs
+++ Virtual Reality Test/Assets/_Scripts/UserController.cs
@@ -7,11 +7,12 @@ public class UserController : MonoBehaviour {
 
 	private Rigidbody rb; 
 	private float speed = 10; 
+	//Unity equivalent of a constant
+	public Vector3 origin { get { return new Vector3(0, 0.5f, 0); } } 
 
 	void Start () {
 		//set reference to Rigidbody component on the same GameObject
 		rb = GetComponent<Rigidbody> ();
-		SaveCSV (); 
 	}
 
 	//Called just before performing any Physics operations [will move by applying forces to Rigidbody]
@@ -23,14 +24,13 @@ public class UserController : MonoBehaviour {
 		Vector3 movement = new Vector3 (horizontalMove, 0.0f, verticalMove); 
 
 		rb.AddForce (movement * speed); 
+		SaveCSV (); 
 	}
-
+		
 	void SaveCSV () {
 		StringBuilder csvcontent = new StringBuilder ();
-		csvcontent.AppendLine ("Name,Age");
-		csvcontent.AppendLine ("Nicole, 19"); 
-		//string csvpath = "/saved_csv.csv";
-		System.IO.File.WriteAllText ("CSVData.csv", csvcontent.ToString());
+		csvcontent.AppendLine (System.DateTime.Now.ToString()); 
+		System.IO.File.AppendAllText ("CSVData.csv", csvcontent.ToString());
 
 	}
 }
