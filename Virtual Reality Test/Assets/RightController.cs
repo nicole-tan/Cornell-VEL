cc99a78316610b29c703450497d200b2b022f638
Nicole Tan <nat34@cornell.edu>
2016-07-13 10:48:51 -0400

    Testing to see if both Left and Right Controllers log output

--- Virtual Reality Test/Assets/RightController.cs
+++ Virtual Reality Test/Assets/RightController.cs
@@ -3,15 +3,14 @@ using System.Collections;
 using System.Text;
 using System.IO;
 
-
-public class LeftController : MonoBehaviour {
+public class RightController : MonoBehaviour {
 
 	void Update () {
 		ClearCSV (); 
 	} 
-	
+
 	// Called for every physics step (a fixed interval between calls)
-	void FixedUpdate () { 
+	void FixedUpdate () {
 		SaveCSV();
 	}
 
@@ -20,13 +19,13 @@ public class LeftController : MonoBehaviour {
 		Vector3 currPos = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
 		Quaternion currRot = new Quaternion (transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w); 
 		csvcontent.AppendLine (System.DateTime.Now.ToString() + ",  Position: " + currPos.ToString() + ",  Rotation: " + currRot.ToString()); 
-		System.IO.File.AppendAllText ("LeftController.csv", csvcontent.ToString());
+		System.IO.File.AppendAllText ("RightController.csv", csvcontent.ToString());
 
 	}
 
 	//If the space button is pressed, previous log of time, movement, and rotation is deleted. 
 	void ClearCSV () { 
 		if (Input.GetKeyDown(KeyCode.Space))  
-			System.IO.File.WriteAllText ("LeftController.csv", string.Empty);
+			System.IO.File.WriteAllText ("RightController.csv", string.Empty);
 	}
 }
