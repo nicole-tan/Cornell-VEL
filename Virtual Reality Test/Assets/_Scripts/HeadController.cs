ce81d6c64ee94d944b4f46508cc32173ebc089fa
Nicole Tan <nat34@cornell.edu>
2016-07-13 10:35:16 -0400

    Checking to see if CSV is created for LeftController movement

---
+++ Virtual Reality Test/Assets/_Scripts/HeadController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class HeadController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Virtual Reality Test/Assets/_Scripts/LeftController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.IO;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class LeftController : MonoBehaviour {
+
+	//SteamVR_TrackedObject trackedObj;
+
+	// Run whether or not the GameObject is enabled 
+	void Awake () {
+		//trackedObj = GetComponent<SteamVR_TrackedObject> (); 
+	
+	}
+	
+	// Called for every physics step (a fixed interval between calls)
+	void FixedUpdate () {
+		//SteamVR_Controller.Device device = SteamVR_Controller.Input ((int)trackedObj.index); 
+		SaveCSV();
+	}
+
+	void SaveCSV () {
+		StringBuilder csvcontent = new StringBuilder ();
+		Vector3 currPos = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+		Quaternion currRot = new Quaternion (transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w); 
+		csvcontent.AppendLine (System.DateTime.Now.ToString() + "  Position: " + currPos.ToString() + "  Rotation: " + currRot.ToString()); 
+		System.IO.File.AppendAllText ("LeftController.csv", csvcontent.ToString());
+
+	}
+}
