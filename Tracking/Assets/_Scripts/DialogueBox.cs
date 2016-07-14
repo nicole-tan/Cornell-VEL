0dcded18b8275e8cf799ed579fc9cab79247837b
Nicole Tan <nat34@cornell.edu>
2016-07-13 22:55:54 -0400

    In the process of creating a dialogue box to name files

---
+++ Tracking/Assets/_Scripts/DialogueBox.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor; 
+
+public class DialogueBox : MonoBehaviour {
+
+	string userName;
+
+	void OnGUI() {
+		userName = EditorGUILayout.TextField ("User Name", userName);
+
+		if (GUILayout.Button ("Save")) {
+			OnClickSaveName (); 
+		
+		} 
+	}
+
+	void OnClickSaveName() {
+
+		userName = userName.Trim ();
+
+		if (string.IsNullOrEmpty (userName)) {
+			EditorUtility.DisplayDialog ("Unable to save name", "Please enter a valid name.", "Close"); 
+			return;
+		}
+
+	}
+}
