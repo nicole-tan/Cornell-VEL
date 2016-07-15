9726cd48e5d16303282a38bf1cc092ccab6d87fb
Nicole Tan <nat34@cornell.edu>
2016-07-15 15:50:34 -0400

    Added Events class back in; hopefully no gravity defying

--- Tracking/Assets/_Scripts/Controller.cs
+++ Tracking/Assets/_Scripts/Controller.cs
@@ -9,7 +9,6 @@ public class Controller : DialogueBox {
 	protected string identifier; 
 	protected string delimeter = ",";
 	public bool isRecording = true; 
-	public bool isEvent = false;
 
 	private string startTime; 
 
@@ -22,6 +21,8 @@ public class Controller : DialogueBox {
 		//replace colon with underscore
 		startTime = startTime.Replace(":", "_");
 	}
+
+	//Creates a string with the milliseconds included
 	private string fixTime () {
 		string currTime = System.DateTime.Now.ToString (); 
 		string newString = currTime + ":" + System.DateTime.Now.Millisecond.ToString(); 
@@ -29,6 +30,7 @@ public class Controller : DialogueBox {
 		return newString; 
 	}
 
+	//Creates the first instance of a CSV file with the appropriate header 
 	public void CreateCSV() {
 		string header = "date-time,h_x,h_y,h_z,h_p,h_y,h_r,r_x,r_y,r_z,r_p,r_y,r_r,l_x,l_y,l_z,l_p,l_y,l_r \n";
 		File.AppendAllText (startTime + "_" + userName + ".csv", header); 
@@ -76,36 +78,4 @@ public class Controller : DialogueBox {
 			SaveCSV ();
 		} 
 	}
-
-	public void EventPress () {
-		if (Input.GetKeyDown (KeyCode.E)) {
-			isEvent = !isEvent; 
-			CheckEvent (); 
-		} 
-		else {
-			CheckEvent ();
-		}
-		
-	}
-
-	public void CheckEvent() {
-		if (isEvent == true) {
-			SaveEvent ();
-		}
-	}
-
-	public void SaveEvent () {
-		StringBuilder csvcontent = new StringBuilder ();
-		Vector3 currPos = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
-		Vector3 currRot = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
-
-		string csvdata = System.DateTime.Now.ToString () + delimeter + currPos [0] + delimeter + currPos [1] +
-			delimeter + currPos [2] + delimeter + currRot [0] + delimeter + currRot [1] + delimeter + currRot [2] +
-			delimeter + identifier;
-		
-		csvcontent.AppendLine (csvdata); 
-		System.IO.File.AppendAllText ("Events.csv", "E has been pressed!" + csvcontent.ToString());
-		
-	}
-		
 }
