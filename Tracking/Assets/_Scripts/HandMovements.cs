ba69a94ca679bf1104c069fe1b0068cf9a81b188
Nicole Tan <nat34@cornell.edu>
2016-07-19 16:32:44 -0400

    Adjusted by adding 1.4 to y, but still feels off because robot is high from ground

--- Tracking/Assets/_Scripts/HandMovements.cs
+++ Tracking/Assets/_Scripts/HandMovements.cs
@@ -3,11 +3,21 @@ using System.Collections;
 
 public class HandMovements : SteamVR_ControllerManager {
 	public GameObject leftHand;
-	public GameObject rightHand; 
+	public GameObject rightHand;
+
+	void Start () {
+		rightHand.transform.position = new Vector3 (-1.8819723f, 1.0269f, 0.10579531f);
+	}
 
 	void Update() {
-		leftHand.transform.position = left.transform.position;
-		rightHand.transform.position = right.transform.position; 
+		leftHand.transform.position = new Vector3(left.transform.position.x, left.transform.position.y + 1.4, left.transform.position.z);
+		rightHand.transform.position = new Vector3(right.transform.position.x, right.transform.position.y + 1.4, right.transform.position.z); 
+		leftHand.transform.rotation = left.transform.rotation;
+		rightHand.transform.rotation = right.transform.rotation;
+
+		//y position can't be controller position because that's too low?
+		//either that or you have to localize
+		//also why does shoulder have attachments, but hands don't?
 	}
 
 
