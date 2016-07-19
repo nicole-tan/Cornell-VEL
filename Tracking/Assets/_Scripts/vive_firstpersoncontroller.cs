9cfca1a0322c29dd2842024d394427ac2e0209d5
Nicole Tan <nat34@cornell.edu>
2016-07-19 11:28:35 -0400

    Followed Japanese tutorial on first person controller for vive

---
+++ Tracking/Assets/_Scripts/vive_firstpersoncontroller.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class vive_firstpersoncontroller : MonoBehaviour {
+
+
+	SteamVR_TrackedObject trackedObj;
+	public GameObject ViveCameraHead;
+	public GameObject FPSController;
+	public float speed = 3;
+
+	void Awake()
+	{
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+
+	void Update()
+	{
+		var device = SteamVR_Controller.Input((int)trackedObj.index);
+
+		//トリガーを浅く握る
+		if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+		{
+			//Debug.Log("GetTouch Trigger");
+			float slowSpeedFactor = 0.5f;
+			Vector3 moveDistance = ViveCameraHead.transform.TransformDirection(Vector3.forward);
+			Vector3 moveDistance2 = new Vector3(moveDistance.x * Time.deltaTime * speed* slowSpeedFactor, 0, moveDistance.z * Time.deltaTime * speed* slowSpeedFactor);
+			FPSController.transform.position += moveDistance2;
+		}
+		//トリガーを深く握る
+		if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+		{
+			//Debug.Log("GetPress Trigger");
+			Vector3 moveDistance = ViveCameraHead.transform.TransformDirection(Vector3.forward);
+			Vector3 moveDistance2 = new Vector3(moveDistance.x * Time.deltaTime * speed, 0, moveDistance.z * Time.deltaTime * speed);
+			FPSController.transform.position += moveDistance2;
+		}
+
+
+	}
+
+}
