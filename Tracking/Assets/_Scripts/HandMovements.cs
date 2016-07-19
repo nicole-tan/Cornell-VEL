26c2dbdddca30c1cfb8e3876eb815016e73debbb
Nicole Tan <nat34@cornell.edu>
2016-07-19 15:39:22 -0400

    Working on left hand movement; no clue if this will do anything

---
+++ Tracking/Assets/_Scripts/HandMovements.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandMovements : SteamVR_ControllerManager {
+
+	void Update() {
+		transform.position = left.transform.position; 
+	}
+
+
+}
