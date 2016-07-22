757e3469a93610da1be3ccc2ac9f7e54ce9deb99
Nicole Tan <nat34@cornell.edu>
2016-07-22 17:09:01 -0400

    Current state

--- Tracking/Assets/_Scripts/HandMovements.cs
+++ Tracking/Assets/_Scripts/HandMovements.cs
@@ -10,15 +10,15 @@ public class HandMovements : SteamVR_ControllerManager {
 	}
 
 	void Update() {
-		leftHand.transform.position = new Vector3(left.transform.position.x, left.transform.position.y + 1.4f, left.transform.position.z);
-		rightHand.transform.position = new Vector3(right.transform.position.x, right.transform.position.y + 1.4f, right.transform.position.z); 
-		leftHand.transform.rotation = left.transform.rotation;
-		rightHand.transform.rotation = right.transform.rotation;
+        leftHand.transform.position = new Vector3(left.transform.position.x, left.transform.position.y, left.transform.position.z);
+        rightHand.transform.position = new Vector3(right.transform.position.x, right.transform.position.y, right.transform.position.z);
+        leftHand.transform.rotation = left.transform.rotation;
+        rightHand.transform.rotation = right.transform.rotation;
 
-		//y position can't be controller position because that's too low?
-		//either that or you have to localize
-		//also why does shoulder have attachments, but hands don't?
-	}
+        //y position can't be controller position because that's too low?
+        //either that or you have to localize
+        //also why does shoulder have attachments, but hands don't?
+    }
 
 
 }
