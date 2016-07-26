3f656f59615d177de20e3ad785b2f4ef9c805d1b
Nicole Tan <nat34@cornell.edu>
2016-07-26 17:19:17 -0400

    Adding reworked versions of hands with improved skeleton rigs

--- Tracking/Assets/_Scripts/Scaling.cs
+++ Tracking/Assets/_Scripts/Scaling.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class Scaling : MonoBehaviour {
 
+	//Depending on location of hmd, you can guess the height of the person
+		//Find the location of the hmd (aka y distance; distance from ground)
+	//Pull data from a source to determine the arm length
+		//Arm length data 
+	//Adjust the arm length accordingly 
+
 	// Use this for initialization
 	void Start () {
 	
