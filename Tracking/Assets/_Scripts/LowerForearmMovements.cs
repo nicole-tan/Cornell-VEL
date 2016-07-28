969eebf5cad22fa0b216cdddd40317d3e650cce3
Nicole Tan <nat34@cornell.edu>
2016-07-28 16:14:36 -0400

    Added a script to left forearm in movingelbow.FBX; just added more judder + spinning and didn't change mesh movements

---
+++ Tracking/Assets/_Scripts/LowerForearmMovements.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LowerForearmMovements : MonoBehaviour {
+
+	public GameObject wrist; 
+		
+	// Update is called once per frame
+	void Update () {
+		changeWristPosition ();
+	
+	}
+
+	//changeWristPosition() gets the current transformation value of the wrist, multiplies the rotation along x by 0.5, and then sets
+	//the lowerForearm's rotation to that new rotation amount 
+	void changeWristPosition() {
+		var wrist_pos = wrist.transform.eulerAngles; 
+		wrist_pos.x = wrist_pos.x * 0.5f; 
+		transform.eulerAngles = wrist_pos; 
+	
+	}
+}
