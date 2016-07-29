863d1dab1e0dd29d5e94e4c098344eb12d808e7d
Nicole Tan <nat34@cornell.edu>
2016-07-29 13:43:20 -0400

    Created script called FootMovement -> feet move with hmd

---
+++ Tracking/Assets/_Scripts/FootMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootMovement : MonoBehaviour {
+
+	public GameObject headset; 
+
+	// Use this for initialization
+	void Start () {
+		setPosition ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		setPosition (); 
+	
+	}
+
+	void setPosition () {
+		var headsetX = headset.transform.position.x; 
+		var headsetY = headset.transform.position.y;
+		var headsetZ = headset.transform.position.z;
+
+		transform.position = new Vector3 (headsetX, headsetY - 1.5f, headsetZ); 
+	}
+}
