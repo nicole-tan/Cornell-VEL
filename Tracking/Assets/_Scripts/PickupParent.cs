eeae7500d46c1c720655f736bd523dc5a6f4d828
Nicole Tan <nat34@cornell.edu>
2016-07-29 16:04:56 -0400

    Working on implementing grip system; grip and throw objects in field

---
+++ Tracking/Assets/_Scripts/PickupParent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+
+public class PickupParent : MonoBehaviour {
+
+	SteamVR_TrackedObject trackedObj;
+
+	void Awake () {
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+
+	void FixedUpdate () {
+		SteamVR_Controller.Device device = SteamVR_Controller.Input ((int)trackedObj.index); 
+
+		//holding trigger down, but not all the way
+		if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("You are touching the trigger!");
+		}
+	} 
+
+
+	void OnTriggerStay(Collider col) {
+	
+	}
+}
