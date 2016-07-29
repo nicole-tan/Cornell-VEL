a9e3bcbee4fad93c75eb1381922f013c9f78fc37
Nicole Tan <nat34@cornell.edu>
2016-07-29 15:24:49 -0400

    Feet rotation now working properly; positioning of feet not always perfect

--- Tracking/Assets/_Scripts/FootMovement.cs
+++ Tracking/Assets/_Scripts/FootMovement.cs
@@ -4,19 +4,24 @@ using System.Collections;
 public class FootMovement : MonoBehaviour {
 
 	public GameObject headset; 
+	private Vector3 initRotation;
+	private Vector3 prevPosition; 
 
 	// Use this for initialization
-	void Start () {
+	void Start () { 
 		setPosition ();
+		transform.rotation = headset.transform.rotation;
+		initRotation = headset.transform.eulerAngles;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		setPosition (); 
+		setPosition ();
+		setRotation (); 
 	
 	}
-
+		
 	void setPosition () {
 		var headsetX = headset.transform.position.x; 
 		var headsetY = headset.transform.position.y;
@@ -24,4 +29,9 @@ public class FootMovement : MonoBehaviour {
 
 		transform.position = new Vector3 (headsetX + 0.1f, headsetY - 1.5f, headsetZ + 0.2f); 
 	}
+
+	void setRotation () {
+		transform.eulerAngles = new Vector3 (initRotation.x, headset.transform.eulerAngles.y, initRotation.z);
+	}
+
 }
