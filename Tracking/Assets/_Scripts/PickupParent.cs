8ad366605db5ccd5a0c87552d62d51550355f038
Nicole Tan <nat34@cornell.edu>
2016-08-01 10:07:39 -0400

    Left hand controller can now pick up sphere in scene because of PickupParent script

--- Tracking/Assets/_Scripts/PickupParent.cs
+++ Tracking/Assets/_Scripts/PickupParent.cs
@@ -16,12 +16,19 @@ public class PickupParent : MonoBehaviour {
 
 		//holding trigger down, but not all the way
 		if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
-			Debug.Log ("You are touching the trigger!");
+			//Debug.Log ("You are touching the trigger!");
 		}
 	} 
 
 
 	void OnTriggerStay(Collider col) {
-	
+		SteamVR_Controller.Device device = SteamVR_Controller.Input ((int)trackedObj.index); 
+		//Debug.Log ("You have collided with " + col.name + "and activated OnTriggerStay");
+		if (device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) {
+			//Debug.Log ("You have collided with " + col.name + "while holding down Touch");
+			//rigidBody will not be affected by the Physics system because it is being moved by our hands 
+			col.attachedRigidbody.isKinematic = true; 
+			col.gameObject.transform.SetParent (this.gameObject.transform);
+		}
 	}
 }
