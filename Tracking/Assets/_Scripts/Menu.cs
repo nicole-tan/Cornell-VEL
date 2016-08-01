8035fe2c822dcc060d4d67b87277946d182012d7
Nicole Tan <nat34@cornell.edu>
2016-08-01 16:30:05 -0400

    Adding beginning of scripts for GUI

---
+++ Tracking/Assets/_Scripts/Menu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+public class Menu : MonoBehaviour {
+	public GameObject input1dropdown; 
+
+	//Initialize as soon as your object 
+
+
+
+}
