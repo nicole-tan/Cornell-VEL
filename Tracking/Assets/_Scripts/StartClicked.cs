6f3a7ab41ebd2d4fb393a31149abec998f442efe
Nicole Tan <nat34@cornell.edu>
2016-08-02 11:15:40 -0400

    Re-wrote specifications; debug messages removed

--- Tracking/Assets/_Scripts/StartClicked.cs
+++ Tracking/Assets/_Scripts/StartClicked.cs
@@ -1,17 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class StartClicked : FirstInpOut {
+public class StartClicked : InputOuput {
 
 	//The chosen controller becomes the parent of the respective chosen model
 	public void ChangeParent() {
-		Debug.Log ("ChangeParent is being called");
-		Debug.Log ("firstOutDropResult is: " + firstOutDropResult);
-		Debug.Log ("firstInpDropResult is: " + firstInpDropResult); 
-		Debug.Log ("secondOutDropResult is: " + secondOutDropResult); 
-		Debug.Log ("secondInpDropResult is: " + secondInpDropResult);
 		firstOutDropResult.transform.parent = firstInpDropResult.transform;
 		secondOutDropResult.transform.parent = secondInpDropResult.transform; 
-
 	}
 }
