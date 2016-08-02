8d2fe7d9dc06afe0c2262dfa20e5333a130eb546
Nicole Tan <nat34@cornell.edu>
2016-08-02 11:36:30 -0400

    Start screen disappears when button is clicked; removal of useless files, changed files to more apropos names

--- Tracking/Assets/_Scripts/StartClicked.cs
+++ Tracking/Assets/_Scripts/StartClicked.cs
@@ -2,10 +2,14 @@
 using System.Collections;
 
 public class StartClicked : InputOuput {
+	private GameObject start_menu;
 
 	//The chosen controller becomes the parent of the respective chosen model
 	public void ChangeParent() {
 		firstOutDropResult.transform.parent = firstInpDropResult.transform;
 		secondOutDropResult.transform.parent = secondInpDropResult.transform; 
+
+		start_menu = GameObject.Find ("/StartMenu");
+		start_menu.SetActive (false);
 	}
 }
