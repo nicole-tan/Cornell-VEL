12293c3e1157908e3e88baba497da6b3b4c91810
Nicole Tan <nat34@cornell.edu>
2016-08-03 10:41:26 -0400

    Make corresponding arm invisible when foot is choesen; make FootMovt script inactive if foot is chosen as output

--- Tracking/Assets/_Scripts/StartClicked.cs
+++ Tracking/Assets/_Scripts/StartClicked.cs
@@ -2,14 +2,28 @@
 using System.Collections;
 
 public class StartClicked : InputOuput {
-	private GameObject start_menu;
+	private GameObject start_menu; 
 
 	//The chosen controller becomes the parent of the respective chosen model
 	public void ChangeParent() {
 		firstOutDropResult.transform.parent = firstInpDropResult.transform;
-		secondOutDropResult.transform.parent = secondInpDropResult.transform; 
+		secondOutDropResult.transform.parent = secondInpDropResult.transform;
+
+		if (firstOutDropResult == leftFoot) {
+			FootChosen (leftHand);
+			leftFoot.GetComponent<FootMovement> ().enabled = false;
+		}
+
+		if (firstOutDropResult == rightFoot) {
+			FootChosen (rightHand); 
+			rightFoot.GetComponent<FootMovement> ().enabled = false;
+		} 
 
 		start_menu = GameObject.Find ("/StartMenu");
 		start_menu.SetActive (false);
 	}
+
+	public void FootChosen(GameObject correspondingArm){
+		correspondingArm.SetActive (false);
+	}	
 }
