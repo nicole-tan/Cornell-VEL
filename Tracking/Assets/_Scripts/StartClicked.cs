94707bb5504337239ef61a0ca429f4c460d81464
Nicole Tan <nat34@cornell.edu>
2016-08-03 11:43:31 -0400

    Left and right models don't show up when feet are chosen

--- Tracking/Assets/_Scripts/StartClicked.cs
+++ Tracking/Assets/_Scripts/StartClicked.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class StartClicked : InputOuput {
 	private GameObject start_menu; 
+	public GameObject leftModel;
+	public GameObject rightModel; 
 
 	//The chosen controller becomes the parent of the respective chosen model
 	public void ChangeParent() {
@@ -12,11 +14,13 @@ public class StartClicked : InputOuput {
 		if (firstOutDropResult == leftFoot || secondOutDropResult == leftFoot) {
 			FootChosen (leftHand);
 			leftFoot.GetComponent<FootMovement> ().enabled = false;
+			leftModel.SetActive (false); 
 		}
 
 		if (firstOutDropResult == rightFoot || secondOutDropResult == rightFoot) {
 			FootChosen (rightHand); 
 			rightFoot.GetComponent<FootMovement> ().enabled = false;
+			rightModel.SetActive (false); 
 		} 
 
 		start_menu = GameObject.Find ("/StartMenu");
