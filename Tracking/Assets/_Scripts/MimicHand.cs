85214078b680291e052e74bceaacebe93e84e62b
Nicole Tan <nat34@cornell.edu>
2016-08-09 10:30:39 -0400

    Using script to mimic IK arm with fullbodyskeleton.mb works, but not well enough --> edit skeleton for full functionality

---
+++ Tracking/Assets/_Scripts/MimicHand.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MimicHand : MonoBehaviour {
+	public GameObject followedObject; 
+
+	// Use this for initialization
+	void Start () {
+		followPosition ();
+		followRotation (); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		followPosition ();
+		followRotation (); 
+	}
+
+	void followPosition() {
+		transform.position = followedObject.transform.position; 
+	}
+
+	void followRotation() {
+		transform.rotation = followedObject.transform.rotation;
+	} 
+}
