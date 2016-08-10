4ed273b5262ca61ecbda7253a0018f7ab9eca927
Nicole Tan <nat34@cornell.edu>
2016-08-10 11:18:51 -0400

    Substituting avatars in while using an IK Ghost arm, but there are still deformation in the avatar's arms

---
+++ Tracking/Assets/_Scripts/RightFootTransform.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RightFootTransform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3 (transform.position.x, transform.position.y - 1.0f, transform.position.z);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (transform.position.x, transform.position.y - 1.0f, transform.position.z);
+	
+	}
+}
