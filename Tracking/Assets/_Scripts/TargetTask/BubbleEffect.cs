0e86f0bcca107b1c3bb97589a97e48205ba80f70
Nicole Tan <nat34@cornell.edu>
2016-08-11 14:37:52 -0400

    Bubbles now spawning through script in StartManager

---
+++ Tracking/Assets/_Scripts/TargetTask/BubbleEffect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleEffect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Move ();
+	
+	}
+
+	IEnumerator Move() {
+		while (true) {
+			GetComponent<Rigidbody> ().AddForce (Random.insideUnitSphere * 10.0f); 
+			yield return new WaitForSeconds (Random.Range (0.5f, 2.5f)); 
+		}
+	}
+}
---
+++ Tracking/Assets/_Scripts/TargetTask/DuplicateSelf.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuplicateSelf : MonoBehaviour {
+	public GameObject bubbles;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 10; i++) {
+			Instantiate (bubbles, new Vector3(i * 2.0f, 0, 0), Quaternion.identity);
+		}
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
