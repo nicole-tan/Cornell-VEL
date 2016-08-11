9576232940aff4e4975a6e42b826c687a1b7d008
Nicole Tan <nat34@cornell.edu>
2016-08-11 14:41:35 -0400

    Bubbles spawning randomly within dimensions of plane

--- Tracking/Assets/_Scripts/TargetTask/DuplicateSelf.cs
+++ Tracking/Assets/_Scripts/TargetTask/DuplicateSelf.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic; 
 
 public class DuplicateSelf : MonoBehaviour {
-	public GameObject bubbles;
+	public GameObject bubble;
+	public GameObject plane; 
+
+
 	// Use this for initialization
 	void Start () {
 		for (int i = 0; i < 10; i++) {
-			Instantiate (bubbles, new Vector3(i * 2.0f, 0, 0), Quaternion.identity);
-		}
+			var newObj = Instantiate (bubble, createRandomPos(), Quaternion.identity);
 
-	
+		}
 	}
 	
 	// Update is called once per frame
@@ -17,5 +20,12 @@ public class DuplicateSelf : MonoBehaviour {
 	
 	}
 
+	//Creates a Vector3 with random coordinates within the limitations of the plane 
+	Vector3 createRandomPos() {
+		float randomX = Random.Range (plane.transform.position.x - plane.transform.localScale.x/2, plane.transform.position.x + plane.transform.localScale.x/2);
+		float randomY = Random.Range (plane.transform.position.y - plane.transform.localScale.y/2, plane.transform.position.y + plane.transform.localScale.y/2);
+		float randomZ = Random.Range (plane.transform.position.z - plane.transform.localScale.z/2, plane.transform.position.z + plane.transform.localScale.z/2);
+		return new Vector3 (randomX, randomY, randomZ); 
+	} 
 
 }
